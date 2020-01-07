9c20d98e32a44b215174c848943243f317c2e489
Văn Tấn Long <vantanlongdhcn3b@gmail.com>
2020-01-07 22:37:17 +0700

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.master')
 @section('container')
 <section class="site-hero overlay" style="background-image: url(images/hero_4.jpg)" data-stellar-background-ratio="0.5">
-      <div class="container">
-        <div class="row site-hero-inner justify-content-center align-items-center">
-          <div class="col-md-10 text-center" data-aos="fade-up">
-            <span class="custom-caption text-uppercase text-white d-block  mb-3">Welcome To SOGO Hotel <span class="fa fa-star text-primary"></span></span>
-            <h1 class="heading">A Best Place To Stay</h1>
-          </div>
-        </div>
+  <div class="container">
+    <div class="row site-hero-inner justify-content-center align-items-center">
+      <div class="col-md-10 text-center" data-aos="fade-up">
+        <span class="custom-caption text-uppercase text-white d-block  mb-3">Welcome To SOGO Hotel <span class="fa fa-star text-primary"></span></span>
+        <h1 class="heading">A Best Place To Stay</h1>
       </div>
+    </div>
+  </div>
 
-      <a class="mouse smoothscroll" href="#next">
-        <div class="mouse-icon">
-          <span class="mouse-wheel"></span>
-        </div>
-      </a>
-    </section>
+  <a class="mouse smoothscroll" href="#next">
+    <div class="mouse-icon">
+      <span class="mouse-wheel"></span>
+    </div>
+  </a>
+</section>
     <!-- END section -->
     <section class="section bg-light pb-0"  >
       <div class="container">
--- resources/views/timkiem.blade.php
+++ resources/views/timkiem.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.master')
 @section('content')
+<section class="site-hero overlay" style="background-image: url(images/hero_4.jpg)" data-stellar-background-ratio="0.5">
+  <div class="container">
+    <div class="row site-hero-inner justify-content-center align-items-center">
+      <div class="col-md-10 text-center" data-aos="fade-up">
+        <span class="custom-caption text-uppercase text-white d-block  mb-3">Welcome To SOGO Hotel <span class="fa fa-star text-primary"></span></span>
+        <h1 class="heading">A Best Place To Stay</h1>
+      </div>
+    </div>
+  </div>
+
+  <a class="mouse smoothscroll" href="#next">
+    <div class="mouse-icon">
+      <span class="mouse-wheel"></span>
+    </div>
+  </a>
+</section>
   <section class="section">
       <div class="container">
         <div class="row justify-content-center text-center mb-5">
